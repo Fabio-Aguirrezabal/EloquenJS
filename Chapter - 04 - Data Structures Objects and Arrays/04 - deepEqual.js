d4a9f231adc8b0758ee2ec8fbd6713a30decedf8
Fabio Aguirrezabal Pintos <fabio.aguirrezabal@outlook.com>
2025-06-02 14:12:44 +0200

    Update 04 - deepEqual.js
    updated version figured out the capter 04 - deep equal function

--- Chapter - 04 - Data Structures Objects and Arrays/04 - deepEqual.js
+++ Chapter - 04 - Data Structures Objects and Arrays/04 - deepEqual.js
@@ -1,32 +1,29 @@
 
 function deepEqual(obj_1, obj_2) {
-    
-    if (typeof (obj_1) == "object" && obj_1 != null &&
-        typeof (obj_2) == "object" && obj_2 != null) {
 
-        var matches = 0;
- 
-        if (Object.keys(obj_1).length === Object.keys(obj_2).length) {
-            Object.keys(obj_1).forEach(key => {
-              
-                if(obj_1[key]==obj_2[key]){               
-                    matches++;
-                    console.log(obj_1[key], ", is: ", obj_2[key]);
-                }
-            });
-            return matches == Object.keys(obj_1).length;
+    if (obj_1 === obj_2) return true; // If there are simple objects and equalm, returns true
+
+    // If they have same length but have are complex objects.
+    if (typeof obj_1 === "object" && obj_1 != null &&
+        typeof obj_2 === "object" && obj_2 != null) {
+
+        let keys_1 = Object.keys(obj_1); // put all the keys in an array for easier iteration.
+        
+        if (keys_1.length !== Object.keys(obj_2).length) return false;
+
+        for (let key of keys_1) {
+            // if KEY is not in OBJ_2 or if DEEPEQUAL(curren node, curren node) returns false [ if (obj_1 === obj_2) return true; ] its because the element is not in there NODE has not orders like array. 
+            if (!key in obj_2 || !deepEqual(obj_1[key], obj_2[key])) return false;
+
+            return true;
+
         }
-    return false;
-
     }
 }
-
-
 
 let obj = { here: { is: "an" }, object: 2 };
 
 console.log(deepEqual(obj, obj)); // → true
-console.log(deepEqual(obj, {here: 1, object: 2})); // → false
-
-
-console.log(deepEqual({ here: { is: "an" }, object: 2 }, { here: { is: "an" }, object: 2 })); // → true
+console.log(deepEqual(obj, { here: 1, object: 2 })); // → false
+// all has been figured out, just needed a nap
+console.log(deepEqual(obj, { object: 2, here: { is: "an" } })); // → true
